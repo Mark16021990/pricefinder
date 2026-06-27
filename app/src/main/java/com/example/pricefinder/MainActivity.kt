abc5a5eefee4c0349a0ccc2cac2f6bff4fa546e9
Mark <andreichenko.mark@gmail.com>
2026-06-27 16:23:02 +0300

    make price cards clickable, open product page

--- app/src/main/java/com/example/pricefinder/MainActivity.kt
+++ app/src/main/java/com/example/pricefinder/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.pricefinder
 
 import android.Manifest
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
